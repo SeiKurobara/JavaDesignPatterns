a5a8e8f53c97e723dc7522cc2338a3e64cd9a1cf
SeiKurobara <129592215+SeiKurobara@users.noreply.github.com>
2024-02-25 23:01:24 +0800

    Anonymous inner class

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,10 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+
+        // !! Lambda syntax: (arguments) -> Expression
+
         // uses the ConsolePrinter class as an Argument
         Print(new ConsolePrinter());
         Print(new ConsolePrinter2());
 
-        
+        // anonymous Inner Class
+        Print(new SampleInterface() {
+            @Override
+            public void ConsoleMessage(String message) {
+                System.out.println("This is a Anonymous Inner Class "+message);
+            }
+        });
+
     }
 
     public static void Print(SampleInterface sampleInterface) {
